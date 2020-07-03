95c51c646e75db5c6b7d447e3087bcee58677341
Chigozirim Chukwu <nobleman.code@gmx.com>
2020-07-03 14:58:24 -0600

    Use star import

--- src/action.ts
+++ src/action.ts
@@ -1,5 +1,5 @@
 import * as core from "@actions/core";
-import github from "@actions/github";
+import * as github from "@actions/github";
 import semverGte from "semver/functions/gte";
 import {
     isSemVersionedRelease,
