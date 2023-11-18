0e581bd7e3b0384fd3342f3e20f5e32c6c14be3c
smac89 <8305511+smac89@users.noreply.github.com>
2023-11-17 21:46:17 -0600

    update node versions and script targets

--- jest.config.ts
+++ jest.config.ts
@@ -4,11 +4,18 @@ import type { JestConfigWithTsJest } from 'ts-jest';
 
 // https://kulshekhar.github.io/ts-jest/docs/getting-started/paths-mapping
 // https://jestjs.io/docs/en/configuration
-
-// https://docs.github.com/en/actions/configuring-and-managing-workflows/using-environment-variables#default-environment-variables
 const config: JestConfigWithTsJest = {
     bail: process.env.CI == 'true' ? 0 : 1,
-    preset: 'ts-jest',
+    transform: {
+        // '^.+\\.[tj]sx?$' to process js/ts with `ts-jest`
+        // '^.+\\.m?[tj]sx?$' to process js/ts/mjs/mts with `ts-jest`
+        '^.+\\.tsx?$': [
+            'ts-jest',
+            {
+                tsconfig: 'tests/tsconfig.json',
+            },
+        ],
+    },
     testEnvironment: 'node',
     roots: ['<rootDir>'],
     moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths, {
