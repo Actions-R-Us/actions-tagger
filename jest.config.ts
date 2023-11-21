15666f6b7fad1a42140b8a82b381546d12b71fa5
smac89 <8305511+smac89@users.noreply.github.com>
2023-11-20 19:41:14 -0600

    update configs

--- jest.config.ts
+++ jest.config.ts
@@ -1,28 +1,40 @@
-import { pathsToModuleNameMapper } from 'ts-jest';
-import { compilerOptions } from './tsconfig.json';
 import type { JestConfigWithTsJest } from 'ts-jest';
+import { pathsToModuleNameMapper } from 'ts-jest';
 
-// https://kulshekhar.github.io/ts-jest/docs/getting-started/paths-mapping
+import { compilerOptions } from './tsconfig.json';
+
 // https://jestjs.io/docs/en/configuration
 const config: JestConfigWithTsJest = {
-    bail: process.env.CI == 'true' ? 0 : 1,
-    transform: {
-        // '^.+\\.[tj]sx?$' to process js/ts with `ts-jest`
-        // '^.+\\.m?[tj]sx?$' to process js/ts/mjs/mts with `ts-jest`
-        '^.+\\.tsx?$': [
-            'ts-jest',
-            {
-                tsconfig: 'tests/tsconfig.json',
-            },
-        ],
-    },
-    testEnvironment: 'node',
-    roots: ['<rootDir>'],
-    moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths, {
-        prefix: '<rootDir>/',
-    }),
-    verbose: true,
-    globalSetup: '<rootDir>/tests/test-setup.ts',
+  bail: process.env.CI === 'true' ? 0 : 1,
+  transform: {
+    // '^.+\\.[tj]sx?$' to process js/ts with `ts-jest`
+    // '^.+\\.m?[tj]sx?$' to process js/ts/mjs/mts with `ts-jest`
+    '^.+\\.tsx?$': [
+      'ts-jest',
+      {
+        tsconfig: 'tests/tsconfig.json',
+        diagnostics: {
+          warnOnly: true,
+        },
+      },
+    ],
+  },
+  testEnvironment: 'node',
+  roots: ['<rootDir>'],
+  // https://kulshekhar.github.io/ts-jest/docs/getting-started/paths-mapping
+  moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths, {
+    prefix: '<rootDir>/',
+  }),
+  verbose: true,
+  setupFilesAfterEnv: ['<rootDir>/tests/setup.ts'],
 };
+
+const githubActionsReporters: JestConfigWithTsJest['reporters'] = [
+  ['github-actions', { silent: false }],
+  'summary',
+];
+if (process.env.GITHUB_ACTIONS === 'true') {
+  config.reporters = githubActionsReporters;
+}
 
 export default config;
---
+++ src/index.d.ts
@@ -0,0 +1,5 @@
+declare namespace NodeJS {
+  interface ProcessEnv {
+    GITHUB_SHA: string;
+  }
+}
